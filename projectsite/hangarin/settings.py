911b1eccd18a1012efe170cd39c1c5183b4085bc
fraaays <202380114@psu.palawan.edu.ph>
2026-04-07 22:30:14 +0800

    fix the static

--- projectsite/hangarin/settings.py
+++ projectsite/hangarin/settings.py
@@ -167,22 +167,22 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': '/static/img/icon-192.png',
+        'src': '/static/images/icon-192.png',
         'sizes': '192x192'
     },
     {
-        'src': '/static/img/icon-512.png',
+        'src': '/static/images/icon-512.png',
         'sizes': '512x512'
     }
 ]
 
 PWA_APP_ICONS_APPLE = [
     {
-        'src': '/static/img/icon-192.png',
+        'src': '/static/images/icon-192.png',
         'sizes': '192x192'
     },
     {
-        'src': '/static/img/icon-512.png',
+        'src': '/static/images/icon-512.png',
         'sizes': '512x512'
     }
 ]
